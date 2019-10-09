ed6eaeedbdd6eade3d113abb7191e05d61bb2a7f
Schwantes <Schwantes.Andrew@principal.com>
2019-10-09 15:27:01 -0500

    fixing pipeline script paths

--- infra/pipeline/scripts/install-infrastructure-dependencies.sh
+++ infra/pipeline/scripts/install-infrastructure-dependencies.sh
@@ -1,7 +1,7 @@
 set -e
 
 git config --global --add url."https://x-access-token:$1@github.com/".insteadOf "https://github.com/"
-python -m pip install -r infra/cdk/requirements_dev.txt
+python -m pip install -r infra/cdk/requirements.txt
 python -m pip install aws-cdk.core
 python -m pip install aws-cdk.aws_s3
 python -m pip install aws-cdk.aws_lambda
