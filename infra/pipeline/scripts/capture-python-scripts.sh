8d9216616d01e4d0fbb09dc6411d4287467a72df
Schwantes <Schwantes.Andrew@principal.com>
2019-10-10 13:30:23 -0500

    add artifact tasks to pipeline

--- infra/pipeline/scripts/capture-python-scripts.sh
+++ infra/pipeline/scripts/capture-python-scripts.sh
@@ -3,7 +3,7 @@ set -e
 mkdir -p ./target
 
 # cp --parents ./infra/cdk/**/*.py ./target
-cp -r ./infra/cdk/*.py ./target/infra/cdk
+cp -r --parents ./infra/cdk/*.py ./target/infra/cdk
 cp ./infra/cdk/*.json ./target/infra/cdk
 cp ./infra/cdk/*.txt ./target/infra/cdk
 cp ./infra/cdk/app.py ./target/infra/cdk
