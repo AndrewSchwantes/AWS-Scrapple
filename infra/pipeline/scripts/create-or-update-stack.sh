f6d9f84b002c1e4860afd51ccfccbe9280e555af
Schwantes <Schwantes.Andrew@principal.com>
2019-10-10 13:44:25 -0500

    add artifact tasks to pipeline

--- infra/pipeline/scripts/create-or-update-stack.sh
+++ infra/pipeline/scripts/create-or-update-stack.sh
@@ -9,4 +9,4 @@ cd infra/cdk
 
 export AWS_DEFAULT_REGION="us-east-1"
 
-cdk deploy --verbose -r arn:aws:iam::911956111630:role/pgi/infra/pgi-infra-stack-exec --require-approval never
+cdk deploy --verbose -r arn:aws:iam::000000000000:role/pgi/infra/pgi-infra-stack-exec --require-approval never
