19c99efae7fdc25204ee69604b4c1bccb23a54fd
Schwantes <Schwantes.Andrew@principal.com>
2019-10-10 12:15:18 -0500

    refactoring pipeline install scripts

--- infra/pipeline/scripts/install-infrastructure-dependencies.sh
+++ infra/pipeline/scripts/install-infrastructure-dependencies.sh
@@ -1,8 +1,8 @@
 set -e
 
-git config --global --add url."https://x-access-token:$1@github.com/".insteadOf "https://github.com/"
+npm install
+
+# Install build dependencies
+sudo npm install -g aws-cdk@1.6.0
+
 python -m pip install -r infra/cdk/requirements.txt
-python -m pip install aws-cdk.core
-python -m pip install aws-cdk.aws_s3
-python -m pip install aws-cdk.aws_lambda
-python -m pip install aws-cdk.aws_logs
